10f19dfbeddbe68c5321f49f6fde3e40ded6c660
kavindusithara <kavinduks98@gmail.com>
2021-09-22 20:42:14 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [image, setImage] = useState("");
+
+  const handleChange = () => {
+    axios
+      .get(
+        "https://api.generated.photos/api/v1/faces?api_key=ZFVdaszHGLP9odjiePkVhA&order_by=random"
+      )
+      .then((res) => {
+        const uri = res.data.faces[0].urls[4][512];
+        uri && setImage(uri);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  return (
+    <div className="App">
+      <h1>Ai Face Generator</h1>
+      {image && <img src={image} alt="AI FACE GENERATOR" />}
+      <button type="button" onClick={handleChange}>
+        Random
+      </button>
+    </div>
+  );
+}
+
+export default App;
